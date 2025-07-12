4bc90f3ce6845f6237c4d98803fd3cb40291d96a
Tai Le <tuantai0625@gmail.com>
2025-07-12 23:37:23 +0700

    feat: add user input

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  function handleChange(event) {
+    const { name, value } = event.target
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }))
+  }
+
+  console.log(userInput)
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            name="initialInvestment"
+            value={userInput.initialInvestment}
+            onChange={handleChange}
+            required
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            name="annualInvestment"
+            value={userInput.annualInvestment}
+            onChange={handleChange}
+            required
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            name="expectedReturn"
+            value={userInput.expectedReturn}
+            onChange={handleChange}
+            required
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            name="duration"
+            value={userInput.duration}
+            onChange={handleChange}
+            required
+          />
+        </p>
+      </div>
+    </section>
+  )
+}
