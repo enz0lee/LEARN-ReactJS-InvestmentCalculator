303efa57581fd19e0fbf55013871d2bda96310e7
Tai Le <tuantai0625@gmail.com>
2025-07-14 00:30:55 +0700

    feat: add result table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,45 @@
+import { calculateInvestmentResults, formatter } from '../util/investment'
+
 export default function Results({ input }) {
-  console.log(input)
-  return <p>Results...</p>
+  const resultsData = calculateInvestmentResults(input)
+  // const initialInvestment = formatter.format(input.initialInvestment)
+  console.log(input, resultsData)
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Invesment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>
+                {formatter.format(
+                  yearData.valueEndOfYear -
+                    input.initialInvestment -
+                    input.annualInvestment * yearData.year
+                )}
+              </td>
+              <td>
+                {formatter.format(
+                  input.initialInvestment +
+                    input.annualInvestment * yearData.year
+                )}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
